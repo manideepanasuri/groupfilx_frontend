35773ae425db50e355d91aa6fd568ec88ba1a17e
Dinesh <dineshsaladi79@gmail.com>
2025-10-21 19:40:32 +0530

    voting

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+function Loader() {
+  return (
+    <div className="flex items-center justify-center space-x-2">
+      <div className="size-3.5 animate-bounce rounded-full bg-current [animation-delay:-0.3s]"></div>
+      <div className="size-3.5 animate-bounce rounded-full bg-current [animation-delay:-0.13s]"></div>
+      <div className="size-3.5 animate-bounce rounded-full bg-current"></div>
+    </div>
+  );
+}
+
+export default Loader;
